f19f40119f8f63765e791a95b8a6ffde78a3e5ad
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-03-30 18:33:52 +0530

    Update PaymentHelper.php

--- src/Helper/PaymentHelper.php
+++ src/Helper/PaymentHelper.php
@@ -199,13 +199,10 @@ class PaymentHelper
         }
         
         $invoicePrepaymentDetails =  [
-              'invoice_bankname'  => utf8_encode($requestData['invoice_bankname']),
-              'invoice_bankplace' => utf8_encode($requestData['invoice_bankplace']),
+              'invoice_bankname'  => $requestData['invoice_bankname'],
+              'invoice_bankplace' => $requestData['invoice_bankplace'],
               'invoice_iban'      => $requestData['invoice_iban'],
-              'invoice_bic'       => $requestData['invoice_bic'],
-              'due_date'          => $requestData['due_date'],
-              'invoice_type'      => $requestData['invoice_type'],
-              'invoice_account_holder' => $requestData['invoice_account_holder']
+              'invoice_bic'       => $requestData['invoice_bic']
                ];
            
         $invoiceDetails = json_encode($invoicePrepaymentDetails);
