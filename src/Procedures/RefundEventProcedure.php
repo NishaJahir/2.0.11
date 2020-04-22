edddfcfd095a3d2ee3e20b4f5311e49aa2216dc4
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-04-22 14:23:11 +0530

    Update RefundEventProcedure.php

--- src/Procedures/RefundEventProcedure.php
+++ src/Procedures/RefundEventProcedure.php
@@ -79,26 +79,34 @@ class RefundEventProcedure
 	    // Checking order type
 	   if ($order->typeId == OrderType::TYPE_CREDIT_NOTE) {
 		$originOrders = $order->orderReferences; 
-		$this->getLogger(__METHOD__)->error('origin', $originOrders);
 		foreach ($order->orderReferences as $orderReference) {
 			$parent_order_id = $orderReference->originOrderId;
-		   $this->getLogger(__METHOD__)->error('parent', $parent_order_id);
+		        $child_order_id = $originOrders->orderId;
+			$order->id = $parent_order_id;
 		}
 	   } 
 	   
-            $payments = pluginApp(\Plenty\Modules\Payment\Contracts\PaymentRepositoryContract::class);  
+           $payments = pluginApp(\Plenty\Modules\Payment\Contracts\PaymentRepositoryContract::class);  
 	   $paymentDetails = $payments->getPaymentsByOrderId($order->id);
-	    $this->getLogger(__METHOD__)->error('payment', $paymentDetails);
+	   $this->getLogger(__METHOD__)->error('payment', $paymentDetails);
 	   $orderAmount = (float) $order->amounts[0]->invoiceTotal;
+	   $parent_order_amount = (float) $paymentDetails[0]->amount;
+	    $this->getLogger(__METHOD__)->error('partial1', $orderAmount);
+	    $this->getLogger(__METHOD__)->error('partial2', $parent_order_amount);
+	    if ($order->typeId == OrderType::TYPE_CREDIT_NOTE && $parent_order_amount > $orderAmount) {
+		$partial_refund_amount =  $parent_order_amount -  $orderAmount;
+		    $this->getLogger(__METHOD__)->error('partial', $partial_refund_amount);
+	    }  
+	    
 	   $paymentKey = $paymentDetails[0]->method->paymentKey;
 	   $key = $this->paymentService->getkeyByPaymentKey($paymentKey);
 	   $parentOrder = $this->transaction->getTransactionData('orderNo', $order->id);
 	    foreach ($paymentDetails[0]->properties as $paymentStatus)
 		{
 		    if($paymentStatus->typeId == 30)
-				  {
-					$status = $paymentStatus->value;
-				  }	
+		  {
+			$status = $paymentStatus->value;
+		  }	
 		}
 	    if ($status == 100)   
 	    { 
@@ -112,16 +120,18 @@ class RefundEventProcedure
 					'key'            => $key, 
 					'refund_request' => 1, 
 					'tid'            => $parentOrder[0]->tid, 
-					 'refund_param'  => (float) $orderAmount * 100 ,
+					 'refund_param'  => !empty($partial_refund_amount) ?  (float) $partial_refund_amount * 100 : (float) $orderAmount * 100,
 					'remote_ip'      => $this->paymentHelper->getRemoteAddress(),
 					'lang'           => 'de'   
 					 ];
 					
 			    $response = $this->paymentHelper->executeCurl($paymentRequestData, NovalnetConstants::PAYPORT_URL);
 				$responseData =$this->paymentHelper->convertStringToArray($response['response'], '&');
+				 $this->getLogger(__METHOD__)->error('response', $responseData);
+
 				if ($responseData['status'] == '100') {
 					$paymentData['currency']    = $paymentDetails[0]->currency;
-					$paymentData['paid_amount'] = (float) $orderAmount;
+					$paymentData['paid_amount'] = !empty($partial_refund_amount) ?  (float) $partial_refund_amount : (float) $orderAmount,
 					$paymentData['tid']         = !empty($responseData['tid']) ? $responseData['tid'] : $parentOrder[0]->tid;
 					$paymentData['order_no']    = $order->id;
 					$paymentData['type']        = 'debit';
