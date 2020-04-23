321ab72ac9dee8f6c224ba92669a36288d911aac
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-04-23 10:37:31 +0530

    Update PaymentHelper.php

--- src/Helper/PaymentHelper.php
+++ src/Helper/PaymentHelper.php
@@ -704,6 +704,6 @@ class PaymentHelper
         $paymentObj = $this->paymentRepository->createPayment($payment);
          $this->getLogger(__METHOD__)->error('refundpayment', $paymentObj);
         $this->assignPlentyPaymentToPlentyOrder($paymentObj, (int)$paymentData['child_order_id']);
-        $this->updatePayments($paymentData['parent_tid'], $paymentData['tid_status'], $paymentData['parent_order_id'], $paymentData['payment_status']);
+        
     }
 }
