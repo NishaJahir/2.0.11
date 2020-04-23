8cbfd5d67ec6e219722618c28dde8feed2fe0a1a
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-04-23 12:11:59 +0530

    Update RefundEventProcedure.php

--- src/Procedures/RefundEventProcedure.php
+++ src/Procedures/RefundEventProcedure.php
@@ -146,7 +146,7 @@ class RefundEventProcedure
 					$paymentData['parent_tid'] = $parentOrder[0]->tid;
 					
 if ($order->typeId == OrderType::TYPE_CREDIT_NOTE) {
-	 $payment = pluginApp(\Plenty\Modules\Payment\Models\Payment::class);
+	 $payment_class = pluginApp(\Plenty\Modules\Payment\Models\Payment::class);
 	 $this->paymentHelper->createRefundPayment($paymentDetails, $paymentData, $transactionComments);
 	 foreach($paymentDetails as $payment){
 	   $payment->status = ($parent_order_amount > $orderAmount) ? Payment::STATUS_PARTIALLY_REFUNDED : Payment::STATUS_REFUNDED;
